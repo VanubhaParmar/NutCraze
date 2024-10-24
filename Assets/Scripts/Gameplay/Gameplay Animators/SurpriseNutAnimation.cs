5b79469350c7a749de1b1d72272f691c24a4a148
Avi Vasani <avi.vasani@appguruz.com>
2024-10-24 17:11:40 +0530

    nut dissolve shader aded

---
+++ Assets/Scripts/Gameplay/Gameplay Animators/SurpriseNutAnimation.cs
@@ -0,0 +1,36 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurpriseNutAnimation : MonoBehaviour
+{
+    [SerializeField] private Renderer _renderer;
+    [SerializeField] private float animationTime;
+    [SerializeField] private string variableName;
+    [SerializeField] private float startValue;
+    [SerializeField] private float endValue;
+
+    [Button]
+    public void DoSurpriceNutRevealAnimation()
+    {
+        StartCoroutine(DoAnimation());
+    }
+
+    IEnumerator DoAnimation()
+    {
+        float i = 0;
+        float rate = 1 / animationTime;
+
+        MaterialPropertyBlock mp = new MaterialPropertyBlock();
+        mp.SetFloat(variableName, startValue);
+
+        while (i < 1)
+        {
+            i += Time.deltaTime * rate;
+            mp.SetFloat(variableName, Mathf.Lerp(startValue, endValue, i));
+            _renderer.SetPropertyBlock(mp);
+            yield return null;
+        }
+    }
+}
